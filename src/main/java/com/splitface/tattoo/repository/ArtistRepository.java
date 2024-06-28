70f179279ca225c7b8fe7b42742205006698d4d3
Spiersy95 <scottspeirsstirling@gmail.com>
2024-06-28 12:12:02 +0100

    Test for GET all artist done

--- src/main/java/com/splitface/tattoo/repository/ArtistRepository.java
+++ src/main/java/com/splitface/tattoo/repository/ArtistRepository.java
@@ -1,7 +1,7 @@
 package com.splitface.tattoo.repository;
 
 import com.splitface.tattoo.models.Artist;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface ArtistRepository extends JpaRepository<Artist, Long> {
+public interface ArtistRepository extends CrudRepository<Artist, Long> {
 }
