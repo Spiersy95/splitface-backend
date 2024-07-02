85b2ec90f1e534c60b22cb3d7025288216cf22cf
Spiersy95 <scottspeirsstirling@gmail.com>
2024-07-02 09:54:12 +0100

    Getting Ion and I on the same page

--- src/main/java/com/splitface/tattoo/repository/TattooRepository.java
+++ src/main/java/com/splitface/tattoo/repository/TattooRepository.java
@@ -1,21 +1,21 @@
 package com.splitface.tattoo.repository;
 
-import com.splitface.tattoo.models.Style;
 import com.splitface.tattoo.models.Tattoo;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import java.util.List;
 
-import java.util.List;
 
-@Repository
 public interface TattooRepository extends CrudRepository<Tattoo, Long> {
 
-    @Query(value = "SELECT * FROM tattoo \n"+
-        "INNER JOIN style_tattoo_mapping stm ON tattoo.id = stm.tattoo_id \n"+
-        "INNER JOIN style ON style.id = stm.style_id \n"+
-        "WHERE style.id = :val", nativeQuery = true)
+    @Query(value = """
+        SELECT tattoo.id, tattoo.design, tattoo.price, tattoo.artist_id\s
+        FROM tattoo
+        INNER JOIN style_tattoo_mapping stm ON tattoo.id = stm.tattoo_id
+        INNER JOIN style ON style.id = stm.style_id
+        WHERE style.id = :val
+       \s""", nativeQuery = true)
     Iterable<Tattoo> getTattoosByStyleId(@Param("val") long id);
     @Query(value = "SELECT * FROM tattoo WHERE arist_id=:artistId", nativeQuery = true)
     List<Tattoo> getTattoosByArtistId( @Param("artistId") Long artistId);
